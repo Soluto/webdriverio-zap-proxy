9b8c481be40e2b1141557fd8b6026f8acc588971
Omer Levi Hevroni <omerl@soluto.com>
2017-03-23 16:50:20 +0200

    added an example on how to exclude urls

--- app/test.sh
+++ app/test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 zap-cli --zap-url http://zap status -t 120
 zap-cli --zap-url http://zap open-url http://nowasp
+
+# exclude urls you want to ignore here
+# zap-cli --zap-url http://zap exclude ".*.ttf"
+
 npm test
 zap-cli --zap-url http://zap report -o /usr/src/wrk/report.html -f html
